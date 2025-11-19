ea78353e3dfcd16de4018e8177f4747260db7872
HuoJialong <qhyhjl@163.com>
2025-11-19 22:47:07 +0800

    类视图get方法的缓存，@method_decorator(cache_page(timeout=60))

--- component/urls.py
+++ component/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path('software/', views.SoftwareView.as_view()),
     path('student/', views.StudentView.as_view()),
     path('student2/', views.StudentView2.as_view()),
-    path('index/', views.index)
+    path('index/', views.index),
+    path('index2/', views.IndexView.as_view()),
 ]
--- component/views.py
+++ component/views.py
@@ -1,5 +1,6 @@
 import random
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
 from django.views import View
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.cache import cache_page
@@ -91,3 +92,10 @@ class StudentView2(ListView):
 def index(request):
     print("done!")
     return HttpResponse("hello")
+
+"""类视图的缓存只会用于get方法"""
+class IndexView(View):
+    @method_decorator(cache_page(timeout=60))
+    def get(self, request):
+        print("类视图执行了")
+        return HttpResponse("hello，成功了")
